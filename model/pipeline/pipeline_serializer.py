41c7f5eae8e58223940dd247ec1407acbeb956d5
Rafał Kobiela <kobiela.rafal@gmail.com>
2020-11-30 20:33:02 +0100

    remove magic number

--- model/pipeline/pipeline_serializer.py
+++ model/pipeline/pipeline_serializer.py
@@ -9,14 +9,14 @@ from model.pipeline.training_metrics import f1_metric, precision_metric, recall_
 
 
 def save_pipeline(pipeline: Pipeline):
+    number_of_neural_networks = 4
     config = Config()
 
-    for i in range(4):
+    for i in range(number_of_neural_networks):
         model = pipeline.steps[1][1].transformers_[i][1].model
         model_path = os.path.join(config.model_directory, f"tf_{i}.h5")
         model.save(model_path)
 
-    for i in range(4):
         pipeline.steps[1][1].transformers[i][1].model = None
         pipeline.steps[1][1].transformers_[i][1].model = None
 
@@ -26,12 +26,13 @@ def save_pipeline(pipeline: Pipeline):
 
 
 def load_pipeline() -> Pipeline:
+    number_of_neural_networks = 4
     config = Config()
     pipeline_path = os.path.join(config.model_directory, "p1.pkl")
     with open(pipeline_path, "rb") as f:
         pipeline = pickle.load(f)
 
-    for i in range(4):
+    for i in range(number_of_neural_networks):
         model_path = os.path.join(config.model_directory, f"tf_{i}.h5")
         model = tf.keras.models.load_model(model_path, custom_objects={'KerasLayer': hub.KerasLayer,
                                                                        "f1_metric": f1_metric,
